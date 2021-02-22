4906f701f3823d45e3a4057c420f4b6186c12621
emilyl <emily.lukas@gmail.com>
2021-02-22 00:14:01 -0800

    custom navigation menu partially implemented

--- app/src/main/java/com/example/chessie/SecondFragment.java
+++ app/src/main/java/com/example/chessie/SecondFragment.java
@@ -4,24 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Menu;
+import android.widget.Toolbar;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 
 public class SecondFragment extends Fragment {
+
+    // variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    Menu menu;
+    TextView textView;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        drawerLayout = view.findViewById(R.id.drawer_layout);
+        navigationView = view.findViewById(R.id.nav_view);
 
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
